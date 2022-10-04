0bfff4a67eda05e3a17af8248aabb4fab847cc8b
Nguyễn Thanh Châu <chaunguyenthanh7@outlook.com>
2022-10-04 22:00:48 +0700

    Add files via upload

--- scheduled.py
+++ scheduled.py
@@ -6,7 +6,7 @@ import schedule # pip install schedule
 
 default_length = 60 # Seconds
 vlcInstance = vlc.Instance("--demux=ts",b"--rtsp-frame-buffer-size=800000",b"--network-caching=1000")
-path = "D://iec_ptit_2022/cvanomaly_detection/xarac/record16h_18h/"
+path = "D://iec_ptit_2022/cvanomaly_detection/xarac/record16h_18h/scheduled/"
 
 #get Preset Path
 def get_path():
@@ -16,9 +16,9 @@ def get_path():
     return path
 
 def get_stream(length = default_length):
-    mediaName = datetime.now().strftime("%m-%d-%Y-%Hh%Mm%Ss") +".mpg"
+    mediaName = datetime.now().strftime("%m-%d-%Y-%Hh%Mm%Ss") +".avi"
     media_record = vlcInstance.media_new("rtsp://iocldg:iocldg123123@14.241.197.248:2006/cam/realmonitor?channel=1&subtype=0")
-    media_record.add_option("sout=#duplicate{dst=file{access=file,mux=mpg,dst="+get_path()+ "/" + mediaName+"},dst=display}")
+    media_record.add_option("sout=#duplicate{dst=file{access=file,mux=avi,dst="+get_path()+ "/" + mediaName+"},dst=display}")
     media = vlcInstance.media_player_new()
     media.set_media(media_record)
     media.play()
